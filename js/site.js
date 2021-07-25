f20090bf7ca0d5760f30663bf9ea907463e3e0b7
BelleCode <Belinda.tiberio@gmail.com>
2021-07-25 11:19:26 -0700

    Working dropdown, can calling JavaScript Function

--- js/site.js
+++ js/site.js
@@ -1,89 +1,92 @@
  // including functions 
-let events = [{
-        event: 'ComicCon',
-        city: 'New York',
-        state: 'New York',
-        attendance: 240000,
-        date: "06/01/2017"
-    },
-    {
-        event: 'ComicCon',
-        city: 'New York',
-        state: 'New York',
-        attendance: 250000,
-        date: "06/01/2018"
-    },
-    {
-        event: 'ComicCon',
-        city: 'New York',
-        state: 'New York',
-        attendance: 2570000,
-        date: "06/01/2019"
-    },
-    {
-        event: 'ComicCon',
-        city: 'San Diego',
-        state: 'California',
-        attendance: 130000,
-        date: "06/01/2017"
-    },
-    {
-        event: 'ComicCon',
-        city: 'San Diego',
-        state: 'California',
-        attendance: 140000,
-        date: "06/01/2018"
-    },
-    {
-        event: 'ComicCon',
-        city: 'San Diego',
-        state: 'California',
-        attendance: 150000,
-        date: "06/01/2019"
-    },
-    {
-        event: 'ComicCon',
-        city: 'Charlotte',
-        state: 'North Carolina',
-        attendance: 40000,
-        date: "06/01/2017"
-    },
-    {
-        event: 'ComicCon',
-        city: 'Charlotte',
-        state: 'North Carolina',
-        attendance: 45000,
-        date: "06/01/2018"
-    },
-    {
-        event: 'ComicCon',
-        city: 'Charlotte',
-        state: 'North Carolina',
-        attendance: 50000,
-        date: "06/01/2019"
-    },
+ let events = [{
+         event: 'ComicCon',
+         city: 'New York',
+         state: 'New York',
+         attendance: 240000,
+         date: "06/01/2017"
+     },
+     {
+         event: 'ComicCon',
+         city: 'New York',
+         state: 'New York',
+         attendance: 250000,
+         date: "06/01/2018"
+     },
+     {
+         event: 'ComicCon',
+         city: 'New York',
+         state: 'New York',
+         attendance: 2570000,
+         date: "06/01/2019"
+     },
+     {
+         event: 'ComicCon',
+         city: 'San Diego',
+         state: 'California',
+         attendance: 130000,
+         date: "06/01/2017"
+     },
+     {
+         event: 'ComicCon',
+         city: 'San Diego',
+         state: 'California',
+         attendance: 140000,
+         date: "06/01/2018"
+     },
+     {
+         event: 'ComicCon',
+         city: 'San Diego',
+         state: 'California',
+         attendance: 150000,
+         date: "06/01/2019"
+     },
+     {
+         event: 'ComicCon',
+         city: 'Charlotte',
+         state: 'North Carolina',
+         attendance: 40000,
+         date: "06/01/2017"
+     },
+     {
+         event: 'ComicCon',
+         city: 'Charlotte',
+         state: 'North Carolina',
+         attendance: 45000,
+         date: "06/01/2018"
+     },
+     {
+         event: 'ComicCon',
+         city: 'Charlotte',
+         state: 'North Carolina',
+         attendance: 50000,
+         date: "06/01/2019"
+     },
 
-];
+ ];
 
-localStorage.setItem('eventsArray', JSON.stringify(events));
-// the default display for all events by specific cities
-var filteredEvents = events;
+ localStorage.setItem('eventsArray', JSON.stringify(events));
+ // the default display for all events by specific cities
+ var filteredEvents = events;
 
-// get a dropdown of specific cities
-function buildDropDown() {
-    let eventDD = document.getElementById("eventDropDown");
+ // get a dropdown of specific cities
+ function buildDropDown() {
+     let eventDD = document.getElementById("eventDropDown");
 
-    //Distinct events from an array of objects that only looks at city
-    curEvent = JSON.parse(localStorage.getItem('eventsArray'));
-    let distinctEvents = [...new Set(curEvent.map(events => events.city))];
+     //Distinct events from an array of objects that only looks at city
+     curEvent = JSON.parse(localStorage.getItem('eventsArray'));
+     let distinctEvents = [...new Set(curEvent.map(events => events.city))];
 
-    let linkHTMLend = `<div class = "dropdown-divider"> </div><a class="dropdown-item" onclick="getEvents(this)"data-string="All">All</a >`
-    let resultHTML = "";
+     let linkHTMLend = `<div class = "dropdown-divider"> </div><a class="dropdown-item" onclick="getEvents(this)"data-string="All">All</a >`
+     // A place to hold the results for displaying
+     let resultHTML = "";
 
-    for (var i; i < distinctEvents.length; i++) {
-        resultHTML += `<a class= "dropdown-item" onclick= "getEvents(this)" data-string= "${distinctEvents[i]}" > $(distinctEvents[i])</a>`;
+     // Run through a list using a for loop, add them together and display on the screen.
+     for (var i = 0; i < distinctEvents.length; i++) {
 
-    }
-    resultHTML += linkHTMLend;
-    eventDD.innerHTML = resultHTML;
-}
+         resultHTML += `<a class= "dropdown-item" onclick= "getEvents(this)" data-string= "${distinctEvents[i]}" > ${distinctEvents[i]}</a>`;
+
+     }
+     resultHTML += linkHTMLend;
+     eventDD.innerHTML = resultHTML;
+ }
